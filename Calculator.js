94cf4db0dd7ac5a0bfdd90144ddaa6b480e01016
Harut <rhmel94@gmail.com>
2021-08-02 13:48:55 +0400

    first

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+
+const numb = document.querySelector(".numbers"),
+      res = document.querySelector(".result");
+
+function numbfunc(e) {
+    numb.value += e;
+    res.value = "";
+};
+
+function equalfunc(e) {
+    res.value += eval(numb.value);
+    numb.value += eval(numb.value);
+    numb.value = "";
+};
+
+function clean(e) {
+    numb.value = "";
+    res.value = "";
+};
